464d5499299dbeb0be489932d2f2dfdd2c1a62d0
prasobhs4 <38857625+prasobhs4@users.noreply.github.com>
2020-05-15 16:09:20 -0500

    initial commit

---
+++ src/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './HomePage.scss'
+
+
+const HomePage = () => {
+
+    return (
+      <div className='homepage'>
+        <div className='directory-menu'>
+         <div className='menu-item'>
+           <div className='content'>
+                  <h1>HATS</h1>
+                  <h2>SHOP NOW</h2> 
+           </div>
+         </div>
+        </div>
+      </div>
+
+     
+      
+    )
+
+}
+
+
+export default HomePage;
